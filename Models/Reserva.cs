94ac9211c7b1ddc118961252540a4b708ad80519
FelipeFerraz98 <felipeferraz1398@gmail.com>
2025-01-02 21:20:53 -0300

    feat: Add capacity check when registering guests

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -15,16 +15,15 @@ namespace DesafioProjetoHospedagem.Models
 
         public void CadastrarHospedes(List<Pessoa> hospedes)
         {
-            // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
-            // *IMPLEMENTE AQUI*
-            if (true)
+            // Verificar se a capacidade da suíte é maior ou igual ao número de hóspedes
+            if (hospedes.Count <= Suite.Capacidade)
             {
                 Hospedes = hospedes;
             }
             else
             {
-                // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
-                // *IMPLEMENTE AQUI*
+                // Lançar uma exceção caso a capacidade seja insuficiente
+                throw new ArgumentException("Número de hóspedes maior do que a capacidade da suíte.");
             }
         }
 
